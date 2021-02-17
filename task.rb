ec098b74831f3fb2f4cf0c14fcc1b6dc2a9bf299
matsumoto <myoshi878@gmail.com>
2021-02-17 16:59:37 +0900

    Q6~10

--- task.rb
+++ task.rb
@@ -38,21 +38,24 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  puts "numbers2"
+  numbers2 = numbers1.map {|n| n*10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -61,7 +64,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -71,14 +75,21 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+   puts "会員番号No,#{i}番目 #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+   foods.each do |foods|
+    if foods.include?["うに"]
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
 end
 
 def q11

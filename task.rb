f8bf4c7fd3f232ad124ef60f65c69fb63506cd74
matsumoto <myoshi878@gmail.com>
2021-02-18 16:51:22 +0900

    Q11~16

--- task.rb
+++ task.rb
@@ -84,26 +84,30 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-   foods.each do |foods|
-    if foods.include?["うに"]
+   foods.each do |food|
+    if food.include?("うに")      
       puts "好物です"
     else
       puts "まぁまぁ好きです"
     end
+   end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten!.uniq!
+  sports.each.with_index(1) do |sport, i|
+    puts "No#{i} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user] [:name]
 end
 
 def q13
@@ -111,14 +115,17 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge(update_data)
+    puts user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data.each_key do |key|
+    puts key
+  end
 end
 
 def q15
@@ -126,7 +133,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.include?(:age) ? "OK" : "NO"
+  puts data2.include?(:age) ? "OK" : "NO"
 end
 
 def q16
@@ -138,6 +146,8 @@ def q16
   ]
 
   # 以下に回答を記載
+users.each { |user|
+  puts "私の名前は#{user[:name]}です。 年齢は#{user[:age]}です。"}
 
 end
 

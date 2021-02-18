9fcd292a17644ce4a3b6550ad4a3018b3e7eecfb
matsumoto <myoshi878@gmail.com>
2021-02-18 22:06:48 +0900

    Q14 修正

--- task.rb
+++ task.rb
@@ -123,9 +123,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  data.each_key do |key|
-    puts key
-  end
+  array = data.keys
+  puts array
 end
 
 def q15
